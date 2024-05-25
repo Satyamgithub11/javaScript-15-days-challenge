25f1f2e304ef12e75c994c2a9f4608bfdd07f8b2
Satyam Kumar <satyamkuamr823@gmail.com>
2024-05-25 20:03:25 +0530

    add

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 // Day 1 in Js  Variable and Data Type
 // here is my first code in javascript
-console.log("Hello World")
+console.log("Welcome to our college");
+console.log("Satyam Kumar");
+console.log("I love javascript")
+
+//
+name = "tony stark";
+console.log(name);
+
+//
+age = 24;
+console.log(age);
+
+//
+price = 45;
+console.log(price)
+
+// 
+x = null;
+console.log(x);
+
+// 
+y = undefined;
+console.log(y);
