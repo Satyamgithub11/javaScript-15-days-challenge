624d0bc40e9d724e4ca2180a6cf865a01ad73c28
Satyam Kumar <satyamkuamr823@gmail.com>
2024-05-27 08:15:30 +0530

    hh

---
+++ jsday2.js
@@ -0,0 +1,66 @@
+// day 2 operators and conditional statement
+
+console.log("hello");  // to comment the single line comment.
+
+//Artimetic operator
+// let a = 5;
+// let b = 4;
+
+// console.log("a =", a, "& b =", b)
+// console.log("a + b =" , a+b);
+// console.log("a * b =", a*b);
+// console.log("a-b =" , a-b);
+// console.log("a / b =" , a/b);
+// console.log("a % b =" , a%b);
+// console.log("a ** b =" , a**b);
+
+//unary operators
+// let a = 6;
+// let b = 3;
+// console.log("a =",a, "& b =", b);
+// a = a-1;
+// b = b + 1;
+// console.log("a =", a); //5
+// console.log("b =" , b);
+
+//comparison operators
+// let a = 4;
+// let b = 4;
+
+// console.log("4 == 4", a==b); // the output is false.
+// console.log("4 != 4", a !=b);
+
+// let a = 5 ;
+// let b = 2;
+
+// console.log("5 != 2" , a!= b); // true
+
+// let c = 5; //number
+// let d = "5"; // string
+
+// console.log("a == b", a == b);
+
+// let a = 5;
+// let b = "5";
+
+//console.log("a == b", a == b);
+//console.log("a === b ", a === b);
+//console.log("a !== b ", a !== b);
+
+// let a = 5;
+// let b = 3;
+// //console.log("a > b ", a > b);
+// console.log("a < b ", a < b);
+// console.log("a >= b ", a >= b);
+// console.log("a <= b ", a <= b);
+
+// question ---logical operators
+let a = 6;
+let b = 5;
+
+let cond1 = a > b ;
+let cond2 = a === 6;
+//console.log("cond1 && cond2 = ", cond1 && cond2);
+console.log("cond1 || cond2 = ", a < b || a === 6);
+console.log(" !(6 < 5) =  ", !(a < b));
+console.log("!(6 < 5) = ", !(a === 6));
