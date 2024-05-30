807bb4c8f4c1e16caa0682a89d9cff1d38d8f6b4
Satyam Kumar <satyamkuamr823@gmail.com>
2024-05-30 09:28:32 +0530

    h

--- jsday2example.js
+++ jsday2example.js
@@ -1,41 +1,41 @@
-// write a javascript function to check whether a number is even or odd
-// here is the 1st method to write this program.
+// operators and conditions statements 
+// 20 question 
+// write a javascript function to check whether a number is even or odd.
 // let number = prompt("Enter a number");
 
-// if (number % 2 === 0) {
-//     console.log("This is Even number")
-// } else {
-//     console.log("This is odd number")
-// }
+// if ( number % 2 === 0) {
+//     console.log("This is even")
+// } else{
+//     console.log("This is odd")
+//}
 
-// here is the 2nd method to write this program.
-function checkEvenorodd(num) {
-    if (num % 2 === 0){
-        return "Even";
-    } else {
-        return "Odd";
-    }
-}
-
-console.log(checkEvenorodd(4));
-console.log(checkEvenorodd(7));
-
-// question 2 ----> write a program to check if a given year is a leap year ? 
+// question 2 -> create a program that check if a given year is leap year
 function isLeapYear(year) {
     if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-        return true ;
+        return true;
     } else {
         return false;
     }
 }
 
 console.log(isLeapYear(2020));
-console.log(isLeapYear(2021));
+console.log(isLeapYear(2022));
+console.log(isLeapYear(2024));
 
-// question 3 ---> write a function that takes three number as parameter and returns the maximum of the three.
-function findMax (num1, num2, num3) {
-    return Math.max(num1 , num2 , num3);
+// write a function that takes three number as parameters and return the maximum of the three.
+function findMax(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
 }
 
-console.log(findMax(10 , 4,8));
-console.log(findMax(-2, 5, 7));
+console.log(findMax(3, 5,8));
+
+// write a program to calculate the factorial of a given number
+function factorial(number){
+    if(number === 0){
+        return 1;
+    } else {
+        return number * factorial(number - 1);
+    }
+}
+console.log(factorial(9))
+console.log(factorial(3))
---
+++ jsday3.js
@@ -0,0 +1,21 @@
+// calculate sum of 1 to 5
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
+// }
+// console.log("sum = ", sum);
+
+// print 1 to 5 
+for (let i = 1; i <= 5; i++){
+    console.log("i = ", i)
+}
+
+// print all even number from 0 to 10
+
+let num = 0;
+while (num <= 10) {
+    console.log(num);
+    num += 2;
+}
+
+
