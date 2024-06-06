887f96671c7bfc99b009a83c6c09c2dd0370fa03
Satyam Kumar <satyamkuamr823@gmail.com>
2024-06-06 08:48:54 +0530

    commit

--- jsday3.js
+++ jsday3.js
@@ -1,4 +1,14 @@
 // question 1 ---> 
-for(let count = 1; count <= 5; count++) {
+for(let count = 1; count <= 100; count++) {
     console.log("Apna College")
 }
+
+console.log("Loop has ended")
+
+// calculate sum of 1 to 5
+let sum = 0;
+for(let i = 1; i <= 4; i++) {
+    sum = sum + i;
+}
+
+console.log("sum =", sum);
